a4317dea86f3a21d33cd335bceaf03ec9baff2d1
unknown <2btd03@IESRAMONLLULL.LOCAL>
2023-01-24 09:51:20 +0100

    enteros

---
+++ enteros.py
@@ -0,0 +1,6 @@
+import math
+
+a=int(input("a"))
+b=int(input("b"))
+c=int(input("c"))
+print((-b*+sqrt(b**2-(4*a*c)/(2*a))))
---
+++ inputyprint.py
@@ -0,0 +1,9 @@
+print("hola don pepito")
+input()
+print("paso usted por mi casa")
+input()
+print("vio usted a mi madre")
+input()
+print("adios don pepito")
+input()
+

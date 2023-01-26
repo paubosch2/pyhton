f7e62953d4113ca9308738276286d6250b300a3a
unknown <2btd03@IESRAMONLLULL.LOCAL>
2023-01-26 13:06:38 +0100

    h

--- enteros.py
+++ enteros.py
@@ -3,4 +3,22 @@ import math
 a=int(input("a"))
 b=int(input("b"))
 c=int(input("c"))
-print((-b*+sqrt(b**2-(4*a*c))/(2*a)))
+disc = int(((b**2)-(4*a*c)))
+
+if disc > 0 :
+    print("La equación tiene 2 soluciones")
+    print((-b*+math.sqrt(b**2-(4*a*c))/(2*a)))
+    print((-b*-math.sqrt(b**2-(4*a*c))/(2*a)))
+
+if disc == 0 :
+    print("La equación tiene 1 solución")
+    print((-b*+math.sqrt(b**2-(4*a*c))/(2*a)))
+
+if disc < 0 :
+    print("ta mal, no tiene solución")
+
+
+
+print((-b*+math.sqrt(b**2-(4*a*c))/(2*a)))
+print((-b*-math.sqrt(b**2-(4*a*c))/(2*a)))
+

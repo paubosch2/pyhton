29d11b88d2151edeeebfe42f28d661a452a17ee3
unknown <2btd03@IESRAMONLLULL.LOCAL>
2023-02-02 13:06:24 +0100

    boof

--- rallas_de_3.py
+++ rallas_de_3.py
@@ -3,6 +3,7 @@ import random
 op=[0,1,2,3,4,5,6,7,8]
 casilla=[" "," "," "," "," "," "," "," "," "]
 seguir=True
+tie=9
 
 '''
 |0|1|2|
@@ -14,69 +15,131 @@ while seguir:
     print("|"+casilla[3]+"|"+casilla[4]+"|"+casilla[5]+"|")
     print("|"+casilla[6]+"|"+casilla[7]+"|"+casilla[8]+"|")
     pos=int(input("Elige una casilla del 0 al 8 "))
-    casilla[pos]="X"
-
-    pc=random.choice(op)
-    if casilla[pc] == "X" == "O":
-        pc=random.choice(op)
+    if pos in op:
+        casilla[pos]="X"
+        op.remove(pos)
     else:
-        casilla[pc]="O"
+        print("Ya está ocupado")
+        pos=int(input("Elige una casilla del 0 al 8 "))
+        casilla[pos]="X"
+        op.remove(pos)
+    
+    
 
     if casilla[0] == casilla[1] == casilla[2] == "X":
         print("Ganaste a un bot, Orgulloso?")
         seguir=False
-    if casilla[0] == casilla[1] == casilla[2] == "O":
-        print("Perdiste jajaja")
-        seguir=False
+        break 
+    
 
     if casilla[3] == casilla[4] == casilla[5] == "X":
         print("Ganaste a un bot, Orgulloso?")
         seguir=False
-    if casilla[3] == casilla[4] == casilla[5] == "O":
-        print("Perdiste jajaja")
-        seguir=False
+        break
+   
 
     if casilla[6] == casilla[7] == casilla[8] == "X":
         print("Ganaste a un bot, Orgulloso?")
         seguir=False
-    if casilla[6] == casilla[7] == casilla[8] == "O":
-        print("Perdiste jajaja")
-        seguir=False
+        break
+   
 
     if casilla[0] == casilla[3] == casilla[6] == "X":
         print("Ganaste a un bot, Orgulloso?")
         seguir=False
-    if casilla[0] == casilla[3] == casilla[6] == "O":
-        print("Perdiste jajaja")
-        seguir=False
+        break
+ 
 
     if casilla[1] == casilla[4] == casilla[7] == "X":
         print("Ganaste a un bot, Orgulloso?")
         seguir=False
-    if casilla[1] == casilla[4] == casilla[7] == "O":
-        print("Perdiste jajaja")
-        seguir=False
+        break
+
 
     if casilla[2] == casilla[5] == casilla[8] == "X":
         print("Ganaste a un bot, Orgulloso?")
         seguir=False
-    if casilla[2] == casilla[5] == casilla[8] == "O":
-        print("Perdiste jajaja")
-        seguir=False
+        break
 
     if casilla[0] == casilla[4] == casilla[8] == "X":
         print("Ganaste a un bot, Orgulloso?")
         seguir=False
-    if casilla[0] == casilla[4] == casilla[8] == "O":
-        print("Perdiste jajaja")
-        seguir=False
+        break
+
 
     if casilla[2] == casilla[4] == casilla[6] == "X":
         print("Ganaste a un bot, Orgulloso?")
         seguir=False
+        break
+
+
+    tie=tie-1
+    if tie==0:
+        seguir=False
+        print("Empate!!!")
+        break
+
+    
+    pc=random.choice(op)
+    casilla[pc]="O"
+    op.remove(pc)
+
+ 
+    if casilla[0] == casilla[1] == casilla[2] == "O":
+        print("Perdiste jajaja")
+        seguir=False
+        break
+
+
+    if casilla[3] == casilla[4] == casilla[5] == "O":
+        print("Perdiste jajaja")
+        seguir=False
+        break
+
+    if casilla[6] == casilla[7] == casilla[8] == "O":
+        print("Perdiste jajaja")
+        seguir=False
+        break
+
+ 
+    if casilla[0] == casilla[3] == casilla[6] == "O":
+        print("Perdiste jajaja")
+        seguir=False
+        break
+
+   
+    if casilla[1] == casilla[4] == casilla[7] == "O":
+        print("Perdiste jajaja")
+        seguir=False
+        break
+
+ 
+    if casilla[2] == casilla[5] == casilla[8] == "O":
+        print("Perdiste jajaja")
+        seguir=False
+        break
+
+ 
+    if casilla[0] == casilla[4] == casilla[8] == "O":
+        print("Perdiste jajaja")
+        seguir=False
+        break
+
+ 
     if casilla[2] == casilla[4] == casilla[6] == "O":
         print("Perdiste jajaja")
         seguir=False
+        break
+    
+    tie=tie-1
+    if tie==0:
+        seguir=False
+        print("Empate!!!")
+        break
+
+
+
+
 
 
 
